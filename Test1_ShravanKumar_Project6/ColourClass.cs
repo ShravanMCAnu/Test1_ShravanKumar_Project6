2c7af5baa616f68d852c03b50016c245bb9ffdd8
ShravanMCAnu <bskmc850@gmail.com>
2022-11-05 16:18:00 +0530

    Add project files.

---
+++ Test1_ShravanKumar_Project6/ColourClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_ShravanKumar_Project6
+{
+    public class ColourClass
+    {
+      
+        public void ColourPrinter(char choice)
+        {
+            switch (choice)
+            {
+                case 'V' or 'v':
+                    Console.WriteLine("Violet");
+                    break;
+                case 'I' or 'i':
+                    Console.WriteLine("Indigo");
+                    break;
+                case 'B' or 'b':
+                    Console.WriteLine("Blue");
+                    break;
+                case 'G' or 'g':
+                    Console.WriteLine("Green");
+                    break;
+                case 'Y' or 'y':
+                    Console.WriteLine("Yellow");
+                    break;
+                case 'O' or 'o':
+                    Console.WriteLine("Orange");
+                    break;
+                case 'R' or 'r':
+                    Console.WriteLine("Red");
+                    break;
+            }
+        }
+       
+
+    }
+}
---
+++ Test1_ShravanKumar_Project6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Test1_ShravanKumar_Project6;
+
+ColourClass objColourClass=new ColourClass();
+Console.WriteLine(" \" V I B G Y O R \" Choice  a Alphabet to Get the colours");
+char choice = Convert.ToChar(Console.ReadLine());
+objColourClass.ColourPrinter(choice);
